b7eb4fb924794b76596b18b7a811e9bc07621477
DZZT2004 <taodaiuwu@gmail.com>
2023-01-21 00:43:31 +0100

    fase 2 finita

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Faker\Generator as Faker;
+use App\Train;
 use Illuminate\Database\Seeder;
 
 class TrainsTableSeeder extends Seeder
@@ -9,8 +11,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 250; $i++) { 
+            $train = new Train();
+            $train->azienda = $faker->randomElement(['FrecciaRossa', 'Italo', 'Regionale']);
+            $train->stazione_di_partenza = $faker->word();
+            $train->stazione_di_arrivo = $faker->word();
+            $train->orario_di_partenza = $faker->time('H:i:s');
+            $train->orario_di_arrivo = $faker->time('H:i:s');
+            $train->codice_Treno = rand(43125,2151561);
+            $train->numero_Carrozze = rand(8,20);
+            $train->in_orario = true;
+            $train->cancellato = false;
+            $train->data_partenza = date('Y-m-d');
+            $train->save();
+        }
     }
 }
